bb09357e39e545400ec59754544812d22d635c3b
sponguss <spongusAmougus@gmail.com>
2022-09-12 11:04:34 -0300

    Modified ModuleScript object

--- objects/ModuleScript.lua
+++ objects/ModuleScript.lua
@@ -7,9 +7,15 @@ function ModuleScript.new(instance)
     moduleScript.Instance = instance
     moduleScript.Constants = getConstants(closure)
     moduleScript.Protos = getProtos(closure)
+    moduleScript.Environment = getSenv(instance)
     moduleScript.returnValue=function()
         return require(instance)
     end
+    xpcall(function()
+        moduleScript.Source = decompileScript(instance)
+    end, function()
+        moduleScript.Source = "There was an issue while decompiling."
+    end)
 
     return moduleScript
 end
