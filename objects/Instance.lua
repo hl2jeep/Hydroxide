f6a7910696c51a4ee5f026cb2c0144659895f47c
sponguss <spongusAmougus@gmail.com>
2022-09-18 23:33:05 -0300

    Complete InstanceViewer (almost)

--- objects/Instance.lua
+++ objects/Instance.lua
@@ -1,4 +1,5 @@
 local instance = {}
+local Methods = import("modules/InstanceViewer")
 
 function instance.new(instance)
     local newInstance = {}
@@ -8,15 +9,8 @@ function instance.new(instance)
     newInstance.Descendants = instance:GetDescendants()
     newInstance.Children = instance:GetChildren()
     newInstance.IsService = (instance.Parent==game)
-    newInstance.Properties={}
-    newInstance.FillProperties=function() -- Prevent slow loading
-        local api=game:GetService("HttpService"):JSONDecode(game:HttpGet("https://anaminus.github.io/rbx/json/api/latest.json"))
-        table.foreach(api, function(_, v)
-            if (v.Class) and (v.Class==typeof(instance) or v.Class==instance.ClassName or instance:IsA(v.Class)) and (not v.Type or v.Type~="Function") then
-                newInstance.Properties[v.Name]=instance[v.Name]
-            end
-        end)
-    end
+    newInstance.Properties=Methods.GetInstanceProperties(instance)
+    newInstance.Methods=Methods.GetInstanceMethods(instance)
     
     return newInstance
 end
--- ui/modules/InstanceViewer.lua
+++ ui/modules/InstanceViewer.lua
@@ -1,3 +1,6 @@
+local TextService = game:GetService("TextService")
+local TweenService = game:GetService("TweenService")
+
 local InstanceViewer={}
 local Methods=import("modules/InstanceViewer")
 local selected={}
@@ -10,14 +13,49 @@ local Base = import("rbxassetid://10922275113").Base
 local Assets = import("rbxassetid://10924729710").InstanceViewer
 
 local Page = Base.Body.Pages.InstanceViewer
-local ServiceList = Page.List
-local ResultContent = ServiceList.Results.Clip
+local InstanceInfo = Page.Info
+local Buttons = InstanceInfo.Buttons
+local contentList = InstanceInfo.Results.Clip.Content
+local serviceList = Page.List
+local ResultContent = serviceList.Results.Clip
 
-local Query=ServiceList.Query
+local Query=serviceList.Query
 local Refresh=Query.Refresh
 local Search=Query.Search
+local Back=InstanceInfo.Back
+
+local infoQuery=InstanceInfo.Results.Query
+local infoRefresh=infoQuery.Refresh
+local infoSearch=infoQuery.Search
+
+local icons={
+    Property="rbxassetid://4666593882",
+    Method="rbxassetid://4666593447",
+    Instance="rbxassetid://4666594723"
+}
+local constants = {
+    fadeLength = TweenInfo.new(0.15),
+    textWidth = Vector2.new(133742069, 20)
+}
+
+local instanceCache={
+    --[[
+    [workspace.Test]={
+        Properties={},
+        Methods={},
+        Descendants={
+            ["hi"]={
+                Properties={},
+                Methods={},
+                Descendants={}
+            }
+        }
+    }
+    ]]
+}
 
 local ServiceList = List.new(ResultContent.Content)
+local instanceInfoList = List.new(contentList)
 local docsContext=ContextMenuButton.new("rbxassetid://4891705738", "Copy Documentation URL")
 ServiceList:BindContextMenu(ContextMenu.new({ docsContext }))
 
@@ -26,9 +64,73 @@ docsContext:SetCallback(function()
     MessageBox.Show("Success", ("%s's documentation URL was copied to your clipboard."):format(selected.instance.Instance.Name), MessageType.OK)
 end)
 
+local function createProperties(index, value, tbnum)
+    local log = Assets.InstanceLog:Clone()
+    local information = log.Information
+    local indexWidth = TextService:GetTextSize(index, 18, "SourceSans", constants.textWidth).X + 8    
+
+    information.Index.Text = tbnum
+    information.Icon.Image= icons.Property
+
+    information.Index.Size = UDim2.new(0, indexWidth, 0, 20)
+    information.Label.Size = UDim2.new(1, -(indexWidth + 20), 1, 0)
+    information.Icon.Position = UDim2.new(0, indexWidth, 0, 2)
+    information.Label.Position = UDim2.new(0, indexWidth + 20, 0, 0)
+
+    information.Label.Text = index
+    
+    ListButton.new(log, instanceInfoList):SetRightCallback(function()
+        selected.selectedBtn={index=index, value=value, log=log, num=tbnum}
+        selected.selectedType="Property"
+    end)
+end
+
+local function createMethods(index, value, tbnum)
+    local log = Assets.InstanceLog:Clone()
+    local information = log.Information
+    local indexWidth = TextService:GetTextSize(index, 18, "SourceSans", constants.textWidth).X + 8    
+
+    information.Index.Text = tbnum
+    information.Icon.Image= icons.Method
+
+    information.Index.Size = UDim2.new(0, indexWidth, 0, 20)
+    information.Label.Size = UDim2.new(1, -(indexWidth + 20), 1, 0)
+    information.Icon.Position = UDim2.new(0, indexWidth, 0, 2)
+    information.Label.Position = UDim2.new(0, indexWidth + 20, 0, 0)
+
+    information.Label.Text = index
+    
+    ListButton.new(log, instanceInfoList):SetRightCallback(function()
+        selected.selectedBtn={index=index, value=value, log=log, num=tbnum}
+        selected.selectedType="Method"
+    end)
+end
+
+local function createDescendants(index, value, tbnum)
+    local log = Assets.InstanceLog:Clone()
+    local information = log.Information
+    local indexWidth = TextService:GetTextSize(index, 18, "SourceSans", constants.textWidth).X + 8    
+
+    information.Index.Text = tbnum
+    information.Icon.Image= icons.Instance
+
+    information.Index.Size = UDim2.new(0, indexWidth, 0, 20)
+    information.Label.Size = UDim2.new(1, -(indexWidth + 20), 1, 0)
+    information.Icon.Position = UDim2.new(0, indexWidth, 0, 2)
+    information.Label.Position = UDim2.new(0, indexWidth + 20, 0, 0)
+
+    information.Label.Text = index
+    
+    ListButton.new(log, instanceInfoList):SetRightCallback(function()
+        selected.selectedBtn={index=index, value=value, log=log, num=tbnum}
+        selected.selectedType="Descendant"
+    end)
+end
+
 -- Log Object
 local Log={}
 function Log.new(instance)
+    local log={}
     local originalInstance=instance.Instance
     local instanceName=originalInstance.Name
     local button = Assets.ServiceLog:Clone()
@@ -40,9 +142,29 @@ function Log.new(instance)
     button.Descendants.Text = #instance.Descendants
     Methods.SetToInstanceIcon(button.Icon, originalInstance)
 
+    listButton:SetCallback(function()
+        if selected.scriptLog~=log then
+            instanceInfoList:Clear()
+
+            InstanceInfo.Visible=true
+            serviceList.Visible=false
+
+            instance.FillProperties()
+            local num=1
+            for i,v in pairs(instance.Properties) do
+                createProperties(i,v, num)
+                num+=1
+            end
+
+            selected.scriptLog=log
+        end
+    end)
+
     listButton:SetRightCallback(function()
         selected.instance=instance
     end)
+    log.Instance = instance
+    log.Button = listButton
 end
 
 -- UI Functionality
@@ -62,6 +184,37 @@ Search.FocusLost:Connect(function(returned)
         addServices(Search.Text)
         Search.Text = ""
     end
+end)
+
+Back.MouseButton1Click:Connect(function()
+    instanceInfoList:Clear()
+
+    InstanceInfo.Visible=false
+    serviceList.Visible=true
+end)
+
+Buttons.Methods.MouseButton1Click:Connect(function()
+    instanceInfoList:Clear()
+    local selectedInstance=selected.scriptLog.Instance
+
+    local num=1
+    for i,v in pairs(selectedInstance.Methods) do
+        createMethods(i, v, num)
+        num+=1
+    end
+    instanceInfoList:Recalculate()
+end)
+
+Buttons.Descendants.MouseButton1Click:Connect(function()
+    instanceInfoList:Clear()
+    local selectedInstance=selected.scriptLog.Instance
+
+    local num=1
+    for i,v in pairs(selectedInstance.Children) do
+        createDescendants(i, v, num)
+        num+=1
+    end
+    instanceInfoList:Recalculate()
 end)
 
 Refresh.MouseButton1Click:Connect(function()
